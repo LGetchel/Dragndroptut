db8f834694bc9338c30ff349d9c64fbfb8436dd5
LGetchel <lkgetchel@gmail.com>
2018-02-23 10:02:41 -0800

    close but not quite

--- dragndrop/src/index.js
+++ dragndrop/src/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import Board from './Board'
+import { observe } from './Game'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class ChessboardTutorialApp extends Component {
+	constructor(props) {
+		super(props)
+		this.unobserve = observe(this.handleChange.bind(this))
+	}
+
+	handleChange(knightPosition) {
+		const nextState = { knightPosition }
+		if (this.state) {
+			this.setState(nextState)
+		} else {
+			this.setState = nextState
+		}
+	}
+
+	componentWillUnmount() {
+		this.unobserve()
+	}
+
+	render() {
+		const { knightPosition } = this.state
+		return (
+			<div>
+				<p>
+					<b>
+						<a href="https://github.com/react-dnd/react-dnd/tree/master/examples/00%20Chessboard/Tutorial%20App">
+							Browse the Source
+						</a>
+					</b>
+				</p>
+				<p>
+					This is a sample app you&apos;ll build as you work through the{' '}
+					<a href="docs-tutorial.html">tutorial</a>.
+				</p>
+				<p>
+					It illustrates creating the drag sources and the drop targets, using
+					the monitors to query the current drag state, and customizing the drag
+					previews.
+				</p>
+				<div
+					style={{
+						width: 500,
+						height: 500,
+						border: '1px solid gray',
+					}}
+				>
+					<Board knightPosition={knightPosition} />
+				</div>
+				<p>
+					Make sure to check out the <a href="docs-tutorial.html">
+						tutorial
+					</a>{' '}
+					for step-by-step instructions on building it!
+				</p>
+			</div>
+		)
+	}
+}
--- dragndrop/src/square.js
+++ dragndrop/src/square.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class Square extends Component {
-  static propTypes = {
-    black: PropTypes.bool
-  };
+	static propTypes = {
+		black: PropTypes.bool,
+		children: PropTypes.node,
+	}
 
-  render() {
-    const { black } = this.props;
-    const fill = black ? 'black' : 'white';
-    const stroke =black ? 'white' : 'black';
+	render() {
+		const { black } = this.props
+		const backgroundColor = black ? 'black' : 'white'
+		const color = black ? 'white' : 'black'
 
-    return <div style={{ 
-        backgroundColor: fill,
-        color: stroke, width: '100%', height: '100%'}}>
-       {this.props.children}
-        </div>;
-  }
+		return (
+			<div
+				style={{
+					color,
+					backgroundColor,
+					width: '100%',
+					height: '100%',
+				}}
+			>
+				{this.props.children}
+			</div>
+		)
+	}
 }
